b7d9dc0a6067c65c77596cbe69c07e23ad868ccf
Abner0907 <wen.huang@codevdynamics.com>
2020-10-16 18:35:06 +0800

    events/set_leds: configure the dp1000's leds' display status

--- src/modules/events/set_leds.cpp
+++ src/modules/events/set_leds.cpp
@@ -40,6 +40,27 @@
 
 #include "status_display.h"
 
+#include <board_config.h>
+#include <px4_log.h>
+#include <matrix/math.hpp>
+#include <drivers/drv_led.h>
+
+#ifndef BOARD_FRONT_LED_MASK
+#define BOARD_FRONT_LED_MASK (0)
+#endif
+
+#ifndef BOARD_REAR_LED_MASK
+#define BOARD_REAR_LED_MASK  (0)
+#endif
+
+#ifndef BOARD_LEFT_LED_MASK
+#define BOARD_LEFT_LED_MASK  (0)
+#endif
+
+#ifndef BOARD_RIGHT_LED_MASK
+#define BOARD_RIGHT_LED_MASK (0)
+#endif
+
 namespace events
 {
 namespace status
@@ -47,7 +68,69 @@ namespace status
 
 void StatusDisplay::set_leds()
 {
-	// Put your LED handling here
+	bool gps_lock_valid = _vehicle_status_flags.condition_global_position_valid;
+	bool home_position_valid = _vehicle_status_flags.condition_home_position_valid;
+	int nav_state = _vehicle_status.nav_state;
+
+	// try to publish the static LED for the first 10s
+	// this avoid the problem if a LED driver did not subscribe to the topic yet
+	if (hrt_absolute_time() < 10 * 1000000 ) {
+
+		// set the base color for front LED
+		_led_control.led_mask = BOARD_FRONT_LED_MASK;
+		_led_control.color = led_control_s::COLOR_WHITE;
+		_led_control.mode = led_control_s::MODE_ON;
+		publish();
+
+	}
+
+	// set the led mask for the status led which are the back LED
+	_led_control.led_mask = BOARD_REAR_LED_MASK;
+
+	if (nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_RTL
+		   || nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_LAND) {
+		_led_control.color = led_control_s::COLOR_YELLOW;
+
+	} else if (nav_state == vehicle_status_s::NAVIGATION_STATE_ALTCTL) {
+		_led_control.color = led_control_s::COLOR_BLUE;
+
+	} else if (nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_MISSION) {
+		_led_control.color = led_control_s::COLOR_PURPLE;
+
+	} else {
+		_led_control.color = led_control_s::COLOR_GREEN;
+	}
+
+	// blink if no GPS and home are set
+	if (gps_lock_valid && home_position_valid) {
+		_led_control.mode = led_control_s::MODE_ON;
+
+	} else {
+		_led_control.mode = led_control_s::MODE_BLINK_NORMAL;
+	}
+
+	// handle battery warnings, once a state is reached it can not be reset
+	if (_battery_status.warning == battery_status_s::BATTERY_WARNING_CRITICAL || _critical_battery) {
+		_led_control.color = led_control_s::COLOR_RED;
+		_led_control.mode = led_control_s::MODE_BLINK_FAST;
+		_critical_battery = true;
+
+	} else if (_battery_status.warning == battery_status_s::BATTERY_WARNING_LOW || _low_battery) {
+		_led_control.color = led_control_s::COLOR_RED;
+		_led_control.mode = led_control_s::MODE_FLASH;
+		_low_battery = true;
+	}
+
+	if (nav_state != _old_nav_state || gps_lock_valid != _old_gps_lock_valid
+	    || home_position_valid != _old_home_position_valid || _battery_status.warning != _old_battery_status_warning) {
+		publish();
+	}
+
+	// copy actual state
+	_old_nav_state = nav_state;
+	_old_gps_lock_valid = gps_lock_valid;
+	_old_home_position_valid = home_position_valid;
+	_old_battery_status_warning = _battery_status.warning;
 }
 
 } /* namespace status */
