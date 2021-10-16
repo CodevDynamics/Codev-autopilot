4d7d80bb5dea12532d6815637cd50cd323c27db4
Abner0907 <wen.huang@codevdynamics.com>
2021-10-16 17:28:49 +0800

    boards: change board id to 1039

--- boards/codev/dp1000-v2/src/hw_config.h
+++ boards/codev/dp1000-v2/src/hw_config.h
@@ -71,7 +71,7 @@
 #define INTERFACE_USART                1
 #define INTERFACE_USART_CONFIG         "/dev/ttyS7,57600"
 #define BOOT_DELAY_ADDRESS             0x000001a0
-#define BOARD_TYPE                     666
+#define BOARD_TYPE                     1039
 #define _FLASH_KBYTES                  (*(uint32_t *)0x1FF1E880)
 #define BOARD_FLASH_SECTORS            (15)
 #define BOARD_FLASH_SIZE               (_FLASH_KBYTES * 1024)
